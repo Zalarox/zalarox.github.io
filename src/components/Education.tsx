3d886fed6c889cfafc70c5386832cd1cf2212bbb
Siddhant Bansal <bansal.siddhant@gmail.com>
2023-05-03 13:35:32 -0400

    Partial changes

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,20 +1,42 @@
 import React from "react";
-import Kitty from "./../static/404_kitty.svg";
+import "./Education.scss";
 
 const Education = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        fontSize: "4rem",
-        color: "lightgrey",
-      }}
-    >
-      <img width="50%" src={Kitty} />
-      Education Page is under construction
+    <div className="education-container">
+      <div className="timeline">
+        {/* Row 1 */}
+        <div className="timeline-empty" />
+        <div className="timeline-middle">
+          <div className="timeline-circle" />
+        </div>
+        <div className="timeline-content">
+          <span className="date">2019 - 2021</span>
+          <span className="degree">Master's in Applied Computer Science</span>
+          <span className="location">Concordia University; (4.17/4.3 GPA)</span>
+        </div>
+
+        {/* Row 2 */}
+        <div className="timeline-content">
+          <h3>Bachelor's in Technology: Computer Science Engineering</h3>
+          <p>Amity University</p>
+        </div>
+        <div className="timeline-middle">
+          <div className="timeline-circle"></div>
+        </div>
+        <div className="timeline-empty" />
+
+        {/* Row 3 */}
+        <div className="timeline-empty" />
+        <div className="timeline-middle fadeout">
+          <div className="timeline-circle" />
+        </div>
+        <div className="timeline-content">
+          <h3>High School: 2009-2013</h3>
+          <p>St. Andrews Scots Sr. Sec. School</p>
+          <p>Physics, Chemistry, Math, Computer Science</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import NavBar from "./components/NavBar";
 import router from "./router";
 
 const root = ReactDOM.createRoot(
